648556c4d8a1f6f840704a5cccb22d161cb1213d
EvelynQueen <quyenneblog@gmail.com>
2024-10-17 23:33:38 +0700

    Add list of time

---
+++ data/TimeList.java
@@ -0,0 +1,13 @@
+package data;
+
+import java.util.ArrayList;
+
+public class TimeList {
+    ArrayList<String> timeList = new ArrayList<>();
+        
+    timeList.add("Hours");
+    timeList.add("Weeks");
+    timeList.add("Days");
+    timeList.add("Months");
+
+}
