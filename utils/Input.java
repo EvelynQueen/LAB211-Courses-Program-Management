59dc9a57d5ab1d232780958090bb4b98a4796e68
EvelynQueen <quyenneblog@gmail.com>
2024-10-17 22:50:06 +0700

    Update input methods

--- utils/Input.java
+++ utils/Input.java
@@ -6,14 +6,49 @@ public class Input {
     private static Scanner sc = new Scanner(System.in);
     // input a unempty string
     public String inputString(String inpMsg, String errMsg){
-        String str;
         while (true) {
-            System.out.println(inpMsg);
-            str = sc.nextLine();
-            if (!str.isEmpty()) { 
+            try {
+                System.out.println(inpMsg);
+                String str = sc.nextLine();
+                if(str.isEmpty()){
+                    throw new Exception();
+                }
                 return str;
+            } catch (Exception e) {
+                System.out.println(errMsg);
             }
-            System.out.println(errMsg);
+        }
+    }
+
+    // input s string mathching regex pattern
+    public String inputString(String inpMsg, String errMsg, String regex){
+        while (true) {
+            try {
+                System.out.println(inpMsg);
+                String str = sc.nextLine();
+                if(str.isEmpty() || !str.matches(regex)){
+                    throw new Exception();
+                }
+                return str;
+            } catch (Exception e) {
+                System.out.println(errMsg);
+            }
+        }
+    };
+
+    // input a posity number
+    public int inputPosInt(String inpMsg, String errMsg){
+        while (true) {
+            try {
+                System.out.println(inpMsg);
+                int num = Integer.parseInt(sc.nextLine());
+                if (num <= 0) {
+                    throw new Exception();
+                }
+                return num;
+            } catch (Exception e) {
+                System.out.println(errMsg);
+            }
         }
     }
 }
