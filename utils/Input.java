3790f2a5129d6dddcad5e3cd1ca8ca8dd48ca121
EvelynQueen <quyenneblog@gmail.com>
2024-10-17 23:58:08 +0700

    Update Input.java

--- utils/Input.java
+++ utils/Input.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Input {
     private static Scanner sc = new Scanner(System.in);
     // input a unempty string
-    public String inputString(String inpMsg, String errMsg){
+    public static String inputString(String inpMsg, String errMsg){
         while (true) {
             try {
                 System.out.println(inpMsg);
@@ -21,7 +21,7 @@ public class Input {
     }
 
     // input s string mathching regex pattern
-    public String inputString(String inpMsg, String errMsg, String regex){
+    public static String inputString(String inpMsg, String errMsg, String regex){
         while (true) {
             try {
                 System.out.println(inpMsg);
@@ -37,7 +37,7 @@ public class Input {
     };
 
     // input a posity number
-    public int inputPosInt(String inpMsg, String errMsg){
+    public static int inputPosInt(String inpMsg, String errMsg){
         while (true) {
             try {
                 System.out.println(inpMsg);
@@ -51,4 +51,32 @@ public class Input {
             }
         }
     }
+
+    // input a number between two given numbers
+    public static int inputPosInt(String inpMsg, String errMsg, int 
+                                    lowerBound, int upperBound){
+        if(lowerBound > upperBound){
+            int tmp = lowerBound;
+            lowerBound = upperBound;
+            upperBound = tmp;
+        }
+        System.out.println(inpMsg); //hiển thị lời mời
+        while (true) {            
+            try {
+                int number = Integer.parseInt(sc.nextLine());
+                if(number < lowerBound || number > upperBound){
+                    throw new Exception();
+                }
+                return number;
+            } catch (Exception e) {
+                System.out.println(errMsg);
+            }
+        }
+    }
 }
+
+/*
+ The methods in the Input class are defined as static (e.g., public static String inputString(...)). 
+  This means you can call these methods without creating an instance of the Input class. 
+  You can simply call them using the class name, like Input.inputString(...).
+ */
